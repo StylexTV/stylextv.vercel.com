f1fc7454599409d3ad6e6c9236d450fc50e9aa4d
StylexTV <stylextv42@gmail.com>
2023-01-06 12:02:42 +0100

    Update StartProjectsListItemLogo.vue

--- components/content/start/projects/list/item/StartProjectsListItemLogo.vue
+++ components/content/start/projects/list/item/StartProjectsListItemLogo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="py-16 lg:p-4 flex items-center justify-center col-span-5">
-    <Image page="start" :path="path" class="w-20 h-20 rounded-lg ring-1 ring-gray-750"/>
+    <Image page="start" :path="path" class="w-20 h-20 rounded-lg ring-1 ring-gray-200 dark:ring-gray-750"/>
   </div>
 </template>
 
