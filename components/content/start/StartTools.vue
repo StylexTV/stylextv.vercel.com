a4e0a38c27bac647689d9327aef6fc5371d8e6f5
StylexTV <stylextv42@gmail.com>
2023-01-06 12:13:39 +0100

    Updated unity logo

--- components/content/start/StartTools.vue
+++ components/content/start/StartTools.vue
@@ -3,7 +3,8 @@
     <StartToolsTitle>Experienced with numerous languages and tools</StartToolsTitle>
 
     <StartToolsList>
-      <StartToolsListItem tool="unity" linkTo="https://unity.com"/>
+      <StartToolsListItem tool="unity/light" linkTo="https://unity.com" class="hidden dark:block"/>
+      <StartToolsListItem tool="unity/dark" linkTo="https://unity.com" class="dark:hidden"/>
       <StartToolsListItem tool="nuxt" linkTo="https://nuxt.com"/>
       <StartToolsListItem tool="vuejs" linkTo="https://vuejs.org"/>
       <StartToolsListItem tool="tailwindcss" linkTo="https://tailwindcss.com"/>
