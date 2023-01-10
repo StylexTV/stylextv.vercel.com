4b5738b3686b5babe5ac0d98333c9be470d4fd55
StylexTV <stylextv42@gmail.com>
2023-01-10 11:38:12 +0100

    Update ErrorHero.vue

--- components/content/error/ErrorHero.vue
+++ components/content/error/ErrorHero.vue
@@ -1,5 +1,5 @@
 <template>
-  <AppContentItem class="min-w-full h-screen flex flex-col items-center justify-center bg-gray-50 dark:bg-black-900">
+  <AppContentItem class="min-w-full h-screen flex flex-col items-center justify-center bg-gray-50 dark:bg-black">
     <ErrorHeroTitle>{{ error.statusCode }}</ErrorHeroTitle>
     <ErrorHeroMessage>{{ statusMessage }}</ErrorHeroMessage>
     <ErrorHeroButton/>
